b5908aaf7eae423aeb4c15bd72cb698c1185530d
TVBushkova <tbeloussova1@yandex.ru>
2025-05-20 16:20:49 +0500

    Работа с авторизацией в Playwright. Практика работы с авторизацией в Playwright

--- playwright_courses.py
+++ playwright_courses.py
@@ -18,6 +18,7 @@ with sync_playwright() as playwright:
     registration_button = page.get_by_test_id('registration-page-registration-button')
     registration_button.click()
 
+    #Сохранеятся состояние браузера
     context.storage_state(path="browser-state.json")
 
     #Открывается страница курсов без повторной авторизации
