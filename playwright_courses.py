6e05554377b042868612364912186fb3a368ad0b
TVBushkova <tbeloussova1@yandex.ru>
2025-05-23 17:56:10 +0500

    Интеграция Pytest и Playwright. Практика написания автотестов

--- playwright_courses.py
+++ playwright_courses.py
@@ -1,4 +1,5 @@
 from playwright.sync_api import sync_playwright, expect
+
 with sync_playwright() as playwright:
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context()
@@ -18,29 +19,26 @@ with sync_playwright() as playwright:
     registration_button = page.get_by_test_id('registration-page-registration-button')
     registration_button.click()
 
-    #Сохранеятся состояние браузера
     context.storage_state(path="browser-state.json")
 
-    #Открывается страница курсов без повторной авторизации
+with sync_playwright() as playwright:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context(storage_state="browser-state.json")
+    page = context.new_page()
+
     page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
 
-    #Проверяется заголовок раздела
-    courses_toolbar_title = page.get_by_test_id('courses-list-toolbar-title-text')
-    expect(courses_toolbar_title).to_be_visible()
-    expect(courses_toolbar_title).to_have_text("Courses")
+    courses_title = page.get_by_test_id('courses-list-toolbar-title-text')
+    expect(courses_title).to_be_visible()
+    expect(courses_title).to_have_text('Courses')
 
-    # Проверяется наличие иконки
-    courses_icon = page.get_by_test_id('courses-list-empty-view-icon')
-    expect(courses_icon).to_be_visible()
+    empty_view_icon = page.get_by_test_id('courses-list-empty-view-icon')
+    expect(empty_view_icon).to_be_visible()
 
-    # Проверяется текст заголовка при отсутствии курсов
-    courses_empty_list_title = page.get_by_test_id('courses-list-empty-view-title-text')
-    expect(courses_empty_list_title).to_be_visible()
-    expect(courses_empty_list_title).to_have_text("There is no results")
+    empty_view_title = page.get_by_test_id('courses-list-empty-view-title-text')
+    expect(empty_view_title).to_be_visible()
+    expect(empty_view_title).to_have_text('There is no results')
 
-    # Проверяется описание раздела при отсутствии курсов
-    courses_description = page.get_by_test_id('courses-list-empty-view-description-text')
-    expect(courses_description).to_be_visible()
-    expect(courses_description).to_have_text("Results from the load test pipeline will be displayed here")
-
-    page.wait_for_timeout(5000)
+    empty_view_description = page.get_by_test_id('courses-list-empty-view-description-text')
+    expect(empty_view_description).to_be_visible()
+    expect(empty_view_description).to_have_text('Results from the load test pipeline will be displayed here')
