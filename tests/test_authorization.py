689423dcffc4ccb8f9d90154a098c33142e94d8a
TVBushkova <tbeloussova1@yandex.ru>
2025-05-25 18:49:58 +0500

    Фикстуры Pytest. Практика работы с pytest фикстурами

--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -1,24 +1,20 @@
-from playwright.sync_api import sync_playwright, expect
+from playwright.sync_api import expect, Page
 import pytest
 
 @pytest.mark.regression
 @pytest.mark.authorisation
-def test_wrong_email_or_password_authorization():
-    with sync_playwright() as playwright:
-        browser = playwright.chromium.launch(headless=False)
-        page = browser.new_page()
+def test_wrong_email_or_password_authorization(initialize_browser_state: Page):
+    initialize_browser_state.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
 
-        page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
+    email_input = initialize_browser_state.locator('//div[@data-testid="login-form-email-input"]//div//input')
+    email_input.fill("user.name@gmail.com")
 
-        email_input = page.locator('//div[@data-testid="login-form-email-input"]//div//input')
-        email_input.fill("user.name@gmail.com")
+    password_input = initialize_browser_state.locator('//div[@data-testid="login-form-password-input"]//div//input')
+    password_input.fill("password")
 
-        password_input = page.locator('//div[@data-testid="login-form-password-input"]//div//input')
-        password_input.fill("password")
+    login_button = initialize_browser_state.locator('//button[@data-testid="login-page-login-button"]')
+    login_button.click()
 
-        login_button = page.locator('//button[@data-testid="login-page-login-button"]')
-        login_button.click()
-
-        wrong_email_or_password_alert = page.locator('//div[@data-testid="login-page-wrong-email-or-password-alert"]')
-        expect(wrong_email_or_password_alert).to_be_visible()
-        expect(wrong_email_or_password_alert).to_have_text("Wrong email or password")
+    wrong_email_or_password_alert = initialize_browser_state.locator('//div[@data-testid="login-page-wrong-email-or-password-alert"]')
+    expect(wrong_email_or_password_alert).to_be_visible()
+    expect(wrong_email_or_password_alert).to_have_text("Wrong email or password")
--- tests/test_registration.py
+++ tests/test_registration.py
@@ -1,29 +1,23 @@
-from playwright.sync_api import sync_playwright, expect
+from playwright.sync_api import Page, expect
 import pytest
 
 @pytest.mark.regression
 @pytest.mark.registration
-def test_successful_registration():  # Создаем тестовую функцию
-    with sync_playwright() as playwright:
-        browser = playwright.chromium.launch(headless=False)
-        page = browser.new_page()
+def test_successful_registration(chromium_page: Page):  # Создаем тестовую функцию
+    chromium_page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
 
-        page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+    email_input = chromium_page.locator('//div[@data-testid="registration-form-email-input"]//div//input')
+    email_input.fill("user.name@gmail.com")
 
-        email_input = page.locator('//div[@data-testid="registration-form-email-input"]//div//input')
-        email_input.fill("user.name@gmail.com")
+    username_input = chromium_page.locator('//div[@data-testid="registration-form-username-input"]//div//input')
+    username_input.fill("username")
 
-        username_input = page.locator('//div[@data-testid="registration-form-username-input"]//div//input')
-        username_input.fill("username")
+    password_input = chromium_page.locator('//div[@data-testid="registration-form-password-input"]//div//input')
+    password_input.fill("password")
 
-        password_input = page.locator('//div[@data-testid="registration-form-password-input"]//div//input')
-        password_input.fill("password")
+    registration_button = chromium_page.locator('//button[@data-testid="registration-page-registration-button"]')
+    registration_button.click()
 
-        registration_button = page.locator('//button[@data-testid="registration-page-registration-button"]')
-        registration_button.click()
-
-        h6_text = page.locator('//h6[@data-testid="dashboard-toolbar-title-text"]')
-        expect(h6_text).to_be_visible()
-        expect(h6_text).to_have_text("Dashboard")
-
-        page.wait_for_timeout(5000)
+    dashboard_title = chromium_page.locator('//h6[@data-testid="dashboard-toolbar-title-text"]')
+    expect(dashboard_title).to_be_visible()
+    expect(dashboard_title).to_have_text("Dashboard")
