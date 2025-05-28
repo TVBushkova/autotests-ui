8c1e0b44b7b5aff65dc5d2f6ef522fb2a0521d49
TVBushkova <tbeloussova1@yandex.ru>
2025-05-29 00:12:02 +0500

    Практикуемся в реализации PageObject. Практикуемся в написании автотестов с использованием POM

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -1,5 +1,6 @@
 from pages.create_course_page import CreateCoursePage
 from pages.courses_list_page import CoursesListPage
+from playwright.sync_api import Page, expect
 import pytest
 
 @pytest.mark.courses
@@ -38,3 +39,23 @@ def test_create_course(create_course_page: CreateCoursePage, courses_list_page: 
     #16
     courses_list_page.check_visible_course_card(index='0', title='Playwright', estimated_time='2 weeks', max_score='100', min_score='10')
 
+
+@pytest.mark.courses
+@pytest.mark.regression
+def test_empty_courses_list(chromium_page_with_state: Page):
+    chromium_page_with_state.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
+
+    courses_title = chromium_page_with_state.get_by_test_id('courses-list-toolbar-title-text')
+    expect(courses_title).to_be_visible()
+    expect(courses_title).to_have_text('Courses')
+
+    empty_view_icon = chromium_page_with_state.get_by_test_id('courses-list-empty-view-icon')
+    expect(empty_view_icon).to_be_visible()
+
+    empty_view_title = chromium_page_with_state.get_by_test_id('courses-list-empty-view-title-text')
+    expect(empty_view_title).to_be_visible()
+    expect(empty_view_title).to_have_text('There is no results')
+
+    empty_view_description = chromium_page_with_state.get_by_test_id('courses-list-empty-view-description-text')
+    expect(empty_view_description).to_be_visible()
+    expect(empty_view_description).to_have_text('Results from the load test pipeline will be displayed here')
