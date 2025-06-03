0f47b344241affa9d09f2e5641a58fd2dd94114b
TVBushkova <tbeloussova1@yandex.ru>
2025-06-03 12:06:05 +0500

    знакомство с PageComponent. практика работы с Page Component

--- pages/dashboard_page.py
+++ pages/dashboard_page.py
@@ -1,13 +1,17 @@
 from playwright.sync_api import Page, expect
-
+from components.navigation.sidebar_component import SidebarComponent
+from components.navigation.navbar_component import NavbarComponent
 from pages.base_page import BasePage
-
 
 class DashboardPage(BasePage):
     def __init__(self, page: Page):
         super().__init__(page)
 
-        # Локаторы элементов страницы
+        # Добавляем компонент Navbar
+        self.navbar = NavbarComponent(page)
+        # Добавляем компонент Sidebar
+        self.sidebar = SidebarComponent(page)
+
         self.dashboard_title = page.get_by_test_id('dashboard-toolbar-title-text')
 
         self.students_title = page.get_by_test_id('students-widget-title-text')
@@ -22,7 +26,6 @@ class DashboardPage(BasePage):
         self.scores_title = page.get_by_test_id('scores-widget-title-text')
         self.scores_chart = page.get_by_test_id('scores-scatter-chart')
 
-        # Метод для проверки отображения заголовка страницы Dashboard
     def check_visible_dashboard_title(self):
         expect(self.dashboard_title).to_be_visible()
         expect(self.dashboard_title).to_have_text('Dashboard')
@@ -32,15 +35,15 @@ class DashboardPage(BasePage):
         expect(self.students_title).to_have_text('Students')
         expect(self.students_chart).to_be_visible()
 
-    def check_visible_activities_chart(self):
-        expect(self.activities_title).to_be_visible()
-        expect(self.activities_title).to_have_text('Activities')
-        expect(self.activities_chart).to_be_visible()
-
     def check_visible_courses_chart(self):
         expect(self.courses_title).to_be_visible()
         expect(self.courses_title).to_have_text('Courses')
         expect(self.courses_chart).to_be_visible()
+
+    def check_visible_activities_chart(self):
+        expect(self.activities_title).to_be_visible()
+        expect(self.activities_title).to_have_text('Activities')
+        expect(self.activities_chart).to_be_visible()
 
     def check_visible_scores_chart(self):
         expect(self.scores_title).to_be_visible()
--- tests/test_courses.py
+++ tests/test_courses.py
@@ -6,56 +6,52 @@ import pytest
 @pytest.mark.courses
 @pytest.mark.regression
 def test_create_course(create_course_page: CreateCoursePage, courses_list_page: CoursesListPage):
-    #1
     create_course_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create")
-    #2
     create_course_page.check_visible_create_course_title()
-    #3
     create_course_page.check_disabled_create_course_button()
-    #4
     create_course_page.check_visible_image_preview_empty_view()
-    #5
     create_course_page.check_visible_image_upload_view()
-    #6
-    create_course_page.check_visible_create_course_form(title='', description='', estimated_time='', max_score='0', min_score='0')
-    #7
+    create_course_page.check_visible_create_course_form(
+        title='',
+        description='',
+        estimated_time='',
+        max_score='0',
+        min_score='0'
+    )
     create_course_page.check_visible_exercises_title()
-    #8
     create_course_page.check_visible_create_exercise_button()
-    #9
     create_course_page.check_visible_exercises_empty_view()
-    #10
     create_course_page.upload_preview_image(file='./testdata/files/image.png')
-    #11
     create_course_page.check_visible_image_upload_view()
-    #12
-    create_course_page.fill_create_course_form(title='Playwright', description='Playwright', estimated_time='2 weeks', max_score='100', min_score='10')
-    #13
+    create_course_page.fill_create_course_form(
+        title='Playwright',
+        description='Playwright',
+        estimated_time='2 weeks',
+        max_score='100',
+        min_score='10'
+    )
     create_course_page.click_create_course_button()
-    #14
     courses_list_page.check_visible_courses_title()
-    #15
     courses_list_page.check_visible_create_course_button()
-    #16
-    courses_list_page.check_visible_course_card(index='0', title='Playwright', estimated_time='2 weeks', max_score='100', min_score='10')
+    courses_list_page.check_visible_course_card(
+        index='0',
+        title='Playwright',
+        estimated_time='2 weeks',
+        max_score='100',
+        min_score='10'
+    )
 
 
 @pytest.mark.courses
 @pytest.mark.regression
-def test_empty_courses_list(chromium_page_with_state: Page):
-    chromium_page_with_state.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
+def test_empty_courses_list(courses_list_page: CoursesListPage):
+    courses_list_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
+    # Добавили проверку Navbar компонента на странице Courses
+    courses_list_page.navbar.check_visible("username")
+    # Добавили проверку Sidebar компонента на странице Courses
+    courses_list_page.sidebar.check_visible()
 
-    courses_title = chromium_page_with_state.get_by_test_id('courses-list-toolbar-title-text')
-    expect(courses_title).to_be_visible()
-    expect(courses_title).to_have_text('Courses')
+    courses_list_page.check_visible_courses_title()
+    courses_list_page.check_visible_create_course_button()
+    courses_list_page.check_visible_empty_view()
 
-    empty_view_icon = chromium_page_with_state.get_by_test_id('courses-list-empty-view-icon')
-    expect(empty_view_icon).to_be_visible()
-
-    empty_view_title = chromium_page_with_state.get_by_test_id('courses-list-empty-view-title-text')
-    expect(empty_view_title).to_be_visible()
-    expect(empty_view_title).to_have_text('There is no results')
-
-    empty_view_description = chromium_page_with_state.get_by_test_id('courses-list-empty-view-description-text')
-    expect(empty_view_description).to_be_visible()
-    expect(empty_view_description).to_have_text('Results from the load test pipeline will be displayed here')
