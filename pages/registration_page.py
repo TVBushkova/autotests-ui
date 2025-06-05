69dd49a74e1b21cce3aff0bca95c89281f232bd2
TVBushkova <tbeloussova1@yandex.ru>
2025-06-05 10:29:13 +0500

    Практикуемся в реализации PageComponent. Практикуемся в написании компонентов с использованием паттерна PageComponent

--- pages/registration_page.py
+++ pages/registration_page.py
@@ -1,5 +1,6 @@
-from playwright.sync_api import Page, expect
+from playwright.sync_api import Page
 
+from components.authentication.registration_form_component import RegistrationFormComponent
 from pages.base_page import BasePage
 
 
@@ -7,29 +8,15 @@ class RegistrationPage(BasePage):
     def __init__(self, page: Page):
         super().__init__(page)
 
-        # Локаторы элементов страницы
-        self.email_input = page.get_by_test_id('registration-form-email-input').locator('input')
-        self.username_input = page.get_by_test_id('registration-form-username-input').locator('input')
-        self.password_input = page.get_by_test_id('registration-form-password-input').locator('input')
         self.registration_button = page.get_by_test_id('registration-page-registration-button')
         self.login_link = page.get_by_test_id('registration-page-login-link')
 
-    # Метод для заполнения формы авторизации
-    def fill_registration_form(self, email: str, username: str, password: str):
-        self.email_input.fill(email)
-        expect(self.email_input).to_have_value(email)
-
-        self.username_input.fill(username)
-        expect(self.username_input).to_have_value(username)
-
-        self.password_input.fill(password)
-        expect(self.password_input).to_have_value(password)
+        self.registration_form = RegistrationFormComponent(page)
 
     # Метод для нажатия на кнопку "Registration"
     def click_registration_button(self):
         self.registration_button.click()
 
-    # Метод для нажатия на ссылку "Login"
+        # Метод для нажатия на ссылку "Login"
     def click_login_link(self):
         self.login_link.click()
-
--- tests/test_registration.py
+++ tests/test_registration.py
@@ -1,20 +1,17 @@
-from playwright.sync_api import Page, expect
 import pytest
-
 from pages.registration_page import RegistrationPage
 from pages.dashboard_page import DashboardPage
-
 
 @pytest.mark.regression
 @pytest.mark.registration
 def test_successful_registration(dashboard_page: DashboardPage, registration_page: RegistrationPage):
     registration_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
-    registration_page.fill_registration_form(
+    registration_page.registration_form.check_visible()
+    registration_page.registration_form.fill(
         email="user.name@gmail.com",
         username="username",
         password="password"
     )
     registration_page.click_registration_button()
-
-    dashboard_page.check_visible_dashboard_title()
+    dashboard_page.dashboard_toolbar_view.check_visible()
 
