50c692d1e57e0a9d4230127a92b5f7fd45859fc7
TVBushkova <tbeloussova1@yandex.ru>
2025-06-17 12:09:01 +0500

    Практические применение PageFactory в автотестах. Практикуемся в применении Page Factory

--- components/courses/create_course_exercises_toolbar_view_component.py
+++ components/courses/create_course_exercises_toolbar_view_component.py
@@ -1,18 +1,20 @@
 from playwright.sync_api import Page, expect
-from components.base_component import BaseComponent
 
+from components.base_component import BaseComponent
+from elements.button import Button
+from elements.text import Text
 
 class CreateCourseExercisesToolbarViewComponent(BaseComponent):
     def __init__(self, page: Page, identifier: str):
         super().__init__(page)
 
-        self.title = page.get_by_test_id(f'{identifier}-title-text')
-        self.create_exercise_button = page.get_by_test_id(f'{identifier}-create-exercise-button')
+        self.title = Text(page,f'{identifier}-title-text', 'Title')
+        self.create_exercise_button = Button(page, f'{identifier}-create-exercise-button', 'Create Exercises')
 
     def check_visible(self):
-        expect(self.title).to_be_visible()
-        expect(self.title).to_have_text('Exercises')
-        expect(self.create_exercise_button).to_be_visible()
+        self.title.check_visible()
+        self.title.check_have_text('Exercises')
+        self.create_exercise_button.check_visible()
 
     def click_create_exercise_button(self):
         self.create_exercise_button.click()
--- components/courses/create_course_toolbar_view_component.py
+++ components/courses/create_course_toolbar_view_component.py
@@ -1,22 +1,24 @@
 from playwright.sync_api import Page, expect
-from components.base_component import BaseComponent
 
+from components.base_component import BaseComponent
+from elements.text import Text
+from elements.button import Button
 
 class CreateCourseToolbarViewComponent(BaseComponent):
     def __init__(self, page: Page, identifier: str):
         super().__init__(page)
 
-        self.title = page.get_by_test_id(f'{identifier}-title-text')
-        self.create_course_button = page.get_by_test_id(f'{identifier}-create-course-button')
+        self.title = Text(page, f'{identifier}-title-text', 'Title')
+        self.create_course_button = Button(page, f'{identifier}-create-course-button', 'Create Course')
 
     def check_visible(self, is_create_course_disabled: bool = True):
-        expect(self.title).to_be_visible()
-        expect(self.title).to_have_text('Create course')
+        self.title.check_visible()
+        self.title.check_have_text('Create course')
         if is_create_course_disabled:
-            expect(self.create_course_button).to_be_disabled()
+            self.create_course_button.check_disabled()
 
         if not is_create_course_disabled:
-            expect(self.create_course_button).not_to_be_disabled()
+            self.create_course_button.check_enabled()
 
     def click_create_course_button(self):
         self.create_course_button.click()
