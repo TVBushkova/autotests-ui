5e2ae73af24afb5597c30f247ca9d683f76e4154
TVBushkova <tbeloussova1@yandex.ru>
2025-07-09 12:30:29 +0500

    огирование взаимодействий со страницей. рактика работы с логированием

--- pages/base_page.py
+++ pages/base_page.py
@@ -2,7 +2,11 @@ import allure
 
 from typing import Pattern
 
-from playwright.sync_api import Page, expect  # Импортируем класс Page
+from playwright.sync_api import Page, expect
+from tools.logger import get_logger  # Импортируем get_logger
+
+logger = get_logger("BASE_PAGE")  # Инициализируем logger
+
 
 class BasePage:
     # Конструктор класса, принимающий объект Page
@@ -10,13 +14,19 @@ class BasePage:
         self.page = page  # Присваиваем объект page атрибуту класса
 
     def visit(self, url):
-        with allure.step(f'Opening the url "{url}"'):  # Добавили allure.step
+        step = f'Opening the url "{url}"'
+        with allure.step(step):  # Добавили allure.step
+            logger.info(step)
             self.page.goto(url, wait_until='networkidle')
 
     def reload(self):
-        with allure.step(f'Reloading page with url "{self.page.url}"'):  # Добавили allure.step
+        step = f'Reloading page with url "{self.page.url}"'
+        with allure.step(step):  # Добавили allure.step
+            logger.info(step)
             self.page.reload(wait_until='domcontentloaded')
 
     def check_current_url(self, expected_url: Pattern[str]):
-        with allure.step(f'Checking that current url matches pattern "{expected_url.pattern}"'): # Добавили allure.step
+        step = f'Checking that current url matches pattern "{expected_url.pattern}"'
+        with allure.step(step): # Добавили allure.step
+            logger.info(step)
             expect(self.page).to_have_url(expected_url)
